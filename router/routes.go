01cbfb6b0d5094c6bbc1fbf10ec2fa2bb6ab00a9
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2024-02-15 10:20:02 -0300

    feat: update handler structere to separate subpackage and create folder cmd

--- router/routes.go
+++ router/routes.go
@@ -1,44 +1,23 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/guilhermeFrBarros/gopportunities/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 
 	rV1 := router.Group("/api/v1")
 	{
-		rV1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Openinng",
-			})
-		})
+		rV1.GET("/opening", handler.CreateOpeningHandler)
 
-		rV1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "POST Openinng",
-			})
-		})
+		rV1.POST("/opening", handler.GetOpeningHandler)
 
-		rV1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "DELETE Openinng",
-			})
-		})
+		rV1.DELETE("/opening", handler.DeleteOpeningHandler)
 
-		rV1.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "PUT Openinng",
-			})
-		})
+		rV1.PUT("/opening", handler.UpdateOpeningHandler)
 
-		rV1.GET("/openingAll", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Openinngs",
-			})
-		})
+		rV1.GET("/openingAll", handler.ListOpeningsHandler)
 	}
 
 }
